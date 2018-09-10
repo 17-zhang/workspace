4a93dd6fb79d340f07ab947085ea1c56497d5319
zhangyiqi0910 <1562070422@qq.com>
2018-09-10 14:17:45 +0800

    ajax总结

---
+++ JavaScript [es5][es6]基础知识/如何创建Ajax/ajax.js
@@ -0,0 +1,18 @@
+// 来源：https://blog.csdn.net/hlinghling/article/details/52624757
+
+/**
+ *  AJAX = 异步的JavaScript+XML
+ */
+
+/**
+ * 详细创建Ajax的过程：
+ *      step:1  创建XMLHttpRequest对象，也就是创建一个异步调用对象；
+ *      step:2  创建一个新的HTTP请求，并指定改HTTP请求的方法、URL以及验证信息；
+ *      step:3  设置响应HTTP状态变化的函数
+ *      step:4  发送HTTP请求
+ *      step:5  获取异步调用返回的数据
+ *      step:6  使用JavaScript和DOM实现局部刷新
+ */
+
+
+
