d73ccd97f1b405b06ea28502ee271fe7efd73559
zyq-Tomato <1562070422@qq.com>
2018-12-05 15:36:24 +0800

    js json动态添加表格

---
+++ jquery ajax json/jquery json动态添加表格/json/testjson.js
@@ -0,0 +1,11 @@
+var testData=[
+	{id:"8",name:"老大",age:"22",height:"172cm",weight:"68kg",sex:"男"},
+	{id:"9",name:"小二",age:"22",height:"177cm",weight:"74g",sex:"男"},
+	{id:"1",name:"张三",age:"22",height:"170cm",weight:"53kg",sex:"男"},
+	{id:"2",name:"李四",age:"18",height:"164cm",weight:"80kg",sex:"女"},
+	{id:"3",name:"王五",age:"37",height:"183cm",weight:"70kg",sex:"男"},
+	{id:"4",name:"六六六六六六六六",age:"50",height:"159cm",weight:"48kg",sex:"女"},
+	{id:"5",name:"七仔",age:"20",height:"143cm",weight:"41kg",sex:"男"},
+	{id:"6",name:"黄八",age:"35",height:"178cm",weight:"49kg",sex:"男"},
+	{id:"7",name:"幺九",age:"19",height:"190cm",weight:"44kg",sex:"女"}
+];
